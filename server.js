4c16d8b732ff146c3557ba956137ef4202d2ff43
Tixerz <petete1296@gmail.com>
2025-03-13 18:18:30 +0330

    basic server setup

---
+++ server.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const express = require('express');
+const PORT = 5000 ; 
+const app  = express();
+
+//middleware
+app.use(express.json()); 
+
+
+app.use(express.static(path.join(__dirname , 'public'))); 
+
+
+app.get('/' , (req, res)=>{
+    res.sendFile(path.join(__dirname , 'public' , 'html-pages' , 'index.html'));
+})
+
+app.listen(PORT , "127.0.0.1" , (err)=>{
+    if(err) return console.log("Failed to run the server");
+    console.log("Listening on port" , PORT);
+})

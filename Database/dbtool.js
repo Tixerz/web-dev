185e1dc86fd9c7a8a9cf590f725d9ef6eda1b4ed
Tixerz <petete1296@gmail.com>
2025-03-13 18:45:17 +0330

    database init

---
+++ Database/dbtool.js
@@ -0,0 +1,13 @@
+const sqlite3 = require('sqlite3');
+let sql;
+
+//connect the db to the file
+const db = new sqlite3.Database("./test.db" , sqlite3.OPEN_READWRITE , (err)=>{
+    if(err)  return console.log(err.message);
+});
+
+//make a user table
+sql = `CREATE TABLE users(id INTIGER PRIMARY KEY , username , password , email)` ;
+db.run(sql , (err)=>{
+    if(err) return console.log(err.message );
+});

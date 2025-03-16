c1add14e781a38e585b886097a0c8517d6057ce9
Tixerz <petete1296@gmail.com>
2025-03-16 16:48:06 +0330

    making the signup endpoint

--- server.js
+++ server.js
@@ -24,7 +24,22 @@ app.get('/' , (req, res)=>{
 });
 app.get('/signup' , (req, res) => {
     res.sendFile(path.join(__dirname , 'public' , 'html-pages' , 'Sign up','sign-up.html'));
-})
+});
+app.post('/signup' , async(req , res) => {
+    let user ;
+    await search_user("./Database/test.db" , req.body.username).then(data=>{
+        user = data;
+    }).catch(data=>{user=data;});
+
+    if(user == undefined){
+        //if username was available
+        await add_user("./Database/test.db",req.body.username , req.body.password , " ");
+        res.send("user created successfuly");
+    }else{
+        //if its not awvailable
+        res.send("user already exist");
+    }
+});
 //entry point for logging in
 app.post('/login' , async (req , res) => {
     try{
